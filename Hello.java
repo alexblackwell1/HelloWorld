b161f1685114fe27f1df1a8b8b6540c71317b6d8
Blackwell  Alexander G <blackwellag>
2021-03-26 17:37:07 -0400

    HelloWorld testing

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/*
+ * Hello.java
+ * Test file made by Alex Blackwell
+ */
+public calss Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
